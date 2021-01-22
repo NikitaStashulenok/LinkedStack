4c32081e5c40dd90ed489a064a4e29266a833ef4
Nikita Stashulenok <nstashylenok@mail.ru>
2021-01-22 21:22:58 +0300

    Добавьте файлы проекта.

---
+++ Model/My_Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinkedStack.Model
+{
+    public class My_Stack<T> : ICloneable
+    {
+        private List<T> items = new List<T>();
+        public int Count => items.Count;
+        public bool IsEmpty => items.Count == 0;
+        public void Push(T item)
+        {
+            items.Add(item);
+        }
+        public T Pop()
+        {
+            if (!IsEmpty)
+            {
+                var item = items.LastOrDefault();
+                items.Remove(item);
+                return item;
+            }
+            else
+            {
+                throw new NullReferenceException("Stack is empty");
+            }
+        }
+        public T Peek()
+        {
+            if (!IsEmpty)
+            {
+                var item = items.LastOrDefault();
+                return item;
+            }
+            else
+            {
+                throw new NullReferenceException("Stack is empty");
+            }
+        }
+        public override string ToString()
+        {
+            return $"In stack -> {Count} elements";
+        }
+
+        public object Clone()
+        {
+            var NewStack = new My_Stack<T>();
+            NewStack.items = new List<T>();
+            return NewStack;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using LinkedStack.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinkedStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new My_Stack<int>();
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(1);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            Stack<string> words = new Stack<string>();
+            words.Push("Water");
+            words.Push("Fire");
+            words.Clear();
+            Console.WriteLine();
+            var arrayStack = new ArrayStack<int>(5);
+            Console.WriteLine(arrayStack.Count);
+            arrayStack.Push(100);
+            arrayStack.Push(200);
+            arrayStack.Push(300);
+            Console.WriteLine(arrayStack.Count);
+            arrayStack.Push(400);
+            arrayStack.Push(500);
+            Console.WriteLine(arrayStack.Pop());
+            Console.WriteLine(arrayStack.Pop());
+            Console.WriteLine(arrayStack.Pop());
+            Console.WriteLine(arrayStack.Pop());
+            Console.WriteLine(arrayStack.Peek());
+
+            Console.ReadKey();
+        }
+    }
+}
